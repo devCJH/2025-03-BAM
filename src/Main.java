6c999c3a913698471c925818d87d450f28efda9c
devCJH <http-@naver.com>
2025-04-01 12:49:29 +0900

    article detail

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,15 @@ class Main {
 		
 		while (true) {
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if (cmd.equals("exit")) {
 				break;
+			}
+			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
 			}
 			
 			if (cmd.equals("article write")) {
@@ -43,8 +48,39 @@ class Main {
 					Article article = articles.get(i);
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+				
+				int id = -1;
+				
+				try {
+					id = Integer.parseInt(cmdBits[2]);
+				} catch (NumberFormatException e) {
+					System.out.println("게시글 번호를 정확하게 입력해주세요");
+					continue;
+				}
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles) {
+					if (id == article.id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시글이 존재하지 않습니다\n", id);
+					continue;
+				}
+				
+				System.out.printf("== %d번 게시글 상세보기 ==\n", foundArticle.id);
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.content);
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
-			
 		}
 
 		System.out.println("== 프로그램 종료 ==");
