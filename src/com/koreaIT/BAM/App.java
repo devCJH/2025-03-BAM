115e72ca8bac0bc0cdaaa900ead00169d3b119db
devCJH <http-@naver.com>
2025-04-03 15:33:06 +0900

    member join

--- src/com/koreaIT/BAM/App.java
+++ src/com/koreaIT/BAM/App.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.koreaIT.BAM.dto.Article;
+import com.koreaIT.BAM.dto.Member;
 import com.koreaIT.BAM.util.Util;
 
 public class App {
 	private int lastArticleId;
 	private List<Article> articles;
+	private int lastMemberId;
+	private List<Member> members;
 
 	public App() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		this.lastMemberId = 0;
+		this.members = new ArrayList<>();
 	}
 
 	public void run() {
@@ -35,8 +40,78 @@ public class App {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
+			if (cmd.equals("member join")) {
+				
+				String loginId = null;
+				String loginPw = null;
+				String name = null;
+				
+				while (true) {
+					System.out.printf("아이디 : ");
+					loginId = sc.nextLine().trim();
+					
+					if (loginId.length() == 0) {
+						System.out.println("아이디는 필수 입력 정보입니다");
+						continue;
+					}
+					
+					boolean isLoginIdDup = false;
+					
+					for (Member member : members) {
+						if (member.getLoginId().equals(loginId)) {
+							isLoginIdDup = true;
+							break;
+						}
+					}
+					
+					if (isLoginIdDup) {
+						System.out.printf("[ %s ]은(는) 이미 사용중인 아이디 입니다\n", loginId);
+						continue;
+					}
+					
+					System.out.printf("[ %s ]은(는) 사용가능한 아이디 입니다\n", loginId);
+					break;
+				}
+				
+				while(true) {
+					System.out.printf("비밀번호 : ");
+					loginPw = sc.nextLine().trim();
+					
+					if (loginPw.length() == 0) {
+						System.out.println("비밀번호는 필수 입력 정보입니다");
+						continue;
+					}
+					
+					System.out.printf("비밀번호 확인 : ");
+					String loginPwChk = sc.nextLine();
+				
+					if (loginPw.equals(loginPwChk) == false) {
+						System.out.println("비밀번호가 일치하지 않습니다");
+						continue;
+					}
+					
+					break;
+				}
+				
+				while (true) {
+					System.out.printf("이름 : ");
+					name = sc.nextLine();
+					
+					if (name.length() == 0) {
+						System.out.println("이름은 필수 입력 정보입니다");
+						continue;
+					}
+					
+					break;
+				}
+				
+				this.lastMemberId++;
 
-			if (cmd.equals("article write")) {
+				Member member = new Member(this.lastMemberId, Util.getDateStr(), loginId, loginPw, name);
+				this.members.add(member);
+
+				System.out.printf("%d번 회원이 가입되었습니다.\n", this.lastMemberId);
+			} else if (cmd.equals("article write")) {
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
